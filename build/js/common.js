289749da60c9b1a5cddaadfa50a66518932a7a0a
Valery Alikin <weblcw@gmail.com>
2018-12-26 00:28:15 +0500

    intial commit

---
+++ build/js/common.js
@@ -0,0 +1,219 @@
+$(function() {
+	//Active menu
+	$('ul li a').click(function(){
+		$('li a').removeClass("active");
+		$(this).addClass("active");
+	});
+
+	//Active filter
+	$('.filter .item').click(function(){
+		$('.filter .item').removeClass("active");
+		$(this).addClass("active");
+	});
+
+	//Toggle menu
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".mnu").fadeToggle();
+		return false;
+	});
+
+
+
+	//Category
+	$(".all_category").click(function() {
+		if($(".s-area").is(":visible")){
+			$(".s-area").slideUp();
+			$(".all_category img").css("transform","rotate(0)");
+		} else {
+			$(".s-area").slideDown();
+			$(".all_category img").css("transform","rotate(180deg)");
+		}
+	});
+
+	//Hide
+	$(".about").click(function() {
+		if($(".info .h2 .hide").is(":visible")){
+			$(".info .h2 .hide").slideUp();
+			$(this).text("Читать больше");
+		} else {
+			$(".info .h2 .hide").slideDown();
+			$(this).text("Скрыть");
+		}
+	});
+
+		//Pen
+		
+		$('.fadestatus').hover(function() {
+			$(this).next(".status").css("display", "inline-block");
+		}, function() {
+			$(this).next(".status").css("display", "none");
+		});
+
+		$('.fadename').hover(function() {
+			$(this).children(".hide").css("display", "inline-block");
+		}, function() {
+			$(this).children(".hide").css("display", "none");
+		});
+
+		$('.t-item-wrap .button-center').click(function() {
+			var item = $(this).parent(".t-item-wrap").children(".hide");
+			if($(item).is(":visible")){
+				$(item).slideUp();
+				$(this).text("Показать больше");
+			} else {
+				$(item).slideDown();
+				$(this).text("Скрыть");
+			}
+		});
+
+		$('.tovar .button-center-all').click(function() {
+			var item = $(this).parent(".tovar").children(".hide");
+			if($(item).is(":visible")){
+				$(item).slideUp();
+				$(this).text("Все категории товаров компании");
+			} else {
+				$(item).slideDown();
+				$(this).text("Скрыть все категории");
+			}
+		});
+
+		$('.rev-show').click(function() {
+			var item = $(this).parent(".right").parent(".bottom").parent(".item").children(".text").children(".hide");
+			if($(item).is(":visible")){
+				$(item).slideUp();
+				$(this).text("Читать полностью");
+			} else {
+				$(item).slideDown();
+				$(this).text("Скрыть");
+			}
+			return false;
+		});		
+
+
+
+
+
+		$('.svg').each(function(){
+			var $img = jQuery(this);
+			var imgID = $img.attr('id');
+			var imgClass = $img.attr('class');
+			var imgURL = $img.attr('src');
+			jQuery.get(imgURL, function(data) {
+				var $svg = jQuery(data).find('svg');
+				if(typeof imgID !== 'undefined') {
+					$svg = $svg.attr('id', imgID);
+				}
+				if(typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+				$svg = $svg.removeAttr('xmlns:a');
+				$img.replaceWith($svg);
+			}, 'xml');
+		});
+
+
+	// sliders
+
+	var swiper = new Swiper('.filter__top', {
+		slidesPerView: 'auto',
+		spaceBetween: 19.7,
+		freeMode: true,
+		navigation: {
+			nextEl: '.filter__left',
+			prevEl: '.filter__right',
+		},
+	});
+
+	var akcii = new Swiper('.akcii.normal .akcii-slider', {
+		slidesPerView: 'auto',
+		spaceBetween: 38.5,
+		freeMode: true,
+		navigation: {
+			nextEl: '.akcii__left',
+			prevEl: '.akcii__right',
+		},
+	});
+
+	$(".rev-slider").slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 3,
+		adaptiveHeight: true,
+		arrows: true,
+		prevArrow:"<div class='prev'><img src='img/icon/small/prev.svg'></div>",
+		nextArrow:"<div class='next'><img src='img/icon/small/next.svg'></div>"
+	});
+
+	$(".artikles-slider").slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 4,
+		adaptiveHeight: true,
+		arrows: true,
+		prevArrow:"<div class='prev'><img src='img/icon/small/prev.svg'></div>",
+		nextArrow:"<div class='next'><img src='img/icon/small/next.svg'></div>"
+	});
+
+	$(".brand-slider").slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 6,
+		adaptiveHeight: true,
+		arrows: true,
+		prevArrow:"<div class='prev'><img src='img/icon/small/prev.svg'></div>",
+		nextArrow:"<div class='next'><img src='img/icon/small/next.svg'></div>"
+	});
+
+	$(".akcii:not(.normal) .akcii-slider").slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 3,
+		adaptiveHeight: true,
+		arrows: true,
+		prevArrow:"<div class='prev'><img src='img/icon/small/prev.svg'></div>",
+		nextArrow:"<div class='next'><img src='img/icon/small/next.svg'></div>"
+	});
+
+	$(".sotrudniki-slider").slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 2,
+		adaptiveHeight: true,
+		arrows: true,
+		prevArrow:"<div class='prev'><img src='img/icon/small/prev.svg'></div>",
+		nextArrow:"<div class='next'><img src='img/icon/small/next.svg'></div>"
+	});	
+
+	$(".zayavki-slider").slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 3,
+		adaptiveHeight: true,
+		arrows: true,
+		prevArrow:"<div class='prev'><img src='img/icon/small/prev.svg'></div>",
+		nextArrow:"<div class='next'><img src='img/icon/small/next.svg'></div>"
+	});
+
+	var activeEl = $('.filter__nav a.active');
+	$('body').on('mouseenter', '.filter__nav a', function () {
+		$('.filter__nav a').removeClass('active');
+	});
+
+	$('body').on('mouseleave', '.filter__nav', function () {
+		activeEl.addClass('active');
+	});
+
+	$('body').on('click', '.filter__nav a', function () {
+		activeEl = $(this);
+		$('.filter__nav a').removeClass('active');
+		activeEl.addClass('active');	
+	});
+
+});
